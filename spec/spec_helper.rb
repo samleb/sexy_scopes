92068bdbc79cbc67c4c801ab634b3dbb79fc99a9
Samuel Lebeau <samuel.lebeau@gmail.com>
2012-10-29 14:10:22 +0100

    Refactor be_extended_by matcher

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -9,7 +9,8 @@ require 'sexy_scopes'
 
 RSpec::Matchers.define :be_extended_by do |expected|
   match do |actual|
-    actual.singleton_class.included_modules.include?(expected)
+    extended_modules = actual.singleton_class.included_modules
+    extended_modules.include?(expected)
   end
 end
 
