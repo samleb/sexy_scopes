2d4dcf617db9e9c08901b23de9f29ae1cda0ebdf
Samuel Lebeau <samuel.lebeau@gmail.com>
2012-10-29 13:00:49 +0100

    Rename literal -> sql_literal (aliased as sql), some documentation added

--- lib/sexy_scopes/active_record.rb
+++ lib/sexy_scopes/active_record.rb
@@ -5,22 +5,36 @@ require 'sexy_scopes/arel'
 
 module SexyScopes
   module ActiveRecord
-    module ClassMethods
-      include Wrappers
-      
-      # Creates an `Attribute` instance for the given <tt>name</tt>.
-      # @return [Attribute] the instance for the given attribute `name`
-      def attribute(name)
-        extend_expression(arel_table[name])
-      end
+    include Wrappers
 
-      # Creates an `SqlLiteral` instance for the given `expression`.
-      def literal(expression)
-        extend_predicate(extend_expression(::Arel.sql(expression)))
-      end
+    # Creates and extends an Arel <tt>Attribute</tt> representing the table's column with
+    # the given <tt>name</tt>.
+    #
+    # @note Please note that no exception will be raised if no such column actually exists.
+    def attribute(name)
+      attribute = arel_table[name]
+      extend_expression(attribute)
     end
 
-    ::ActiveRecord::Base.extend ClassMethods
-
+    # Creates and extends an Arel <tt>SqlLiteral</tt> instance for the given <tt>expression</tt>,
+    # first converted to a string using <tt>to_s</tt>
+    #
+    # == Example
+    #   def Circle.with_perimeter_smaller_than(perimeter)
+    #     where sql_literal(2 * Math::PI) * radius < perimeter
+    #   end
+    #   Circle.with_perimeter_smaller_than(20)
+    #   # SELECT "circles".* FROM "circles" WHERE (6.283185307179586 * "circles"."radius" < 20)
+    #
+    def sql_literal(expression)
+      ::Arel.sql(expression.to_s).tap do |literal|
+        extend_expression(literal)
+        extend_predicate(literal)
+      end
+    end
+    alias_method :sql, :sql_literal
   end
+  
+  # Add these methods to Active Record
+  ::ActiveRecord::Base.extend SexyScopes::ActiveRecord
 end
--- spec/sexy_scopes_spec.rb
+++ spec/sexy_scopes_spec.rb
@@ -1,32 +1,35 @@
 require File.expand_path(File.dirname(__FILE__) + '/spec_helper')
 
 describe SexyScopes::ActiveRecord do
-  it "extends ActiveRecord::Base with ClassMethods" do
-    ActiveRecord::Base.should be_extended_by(SexyScopes::ActiveRecord::ClassMethods)
+  it "should extend ActiveRecord::Base" do
+    ActiveRecord::Base.should be_extended_by SexyScopes::ActiveRecord
   end
   
-  describe "ClassMethods" do
-    describe ".attribute(name)" do
-      subject { User.attribute(:username) }
-      
-      it "should return an Arel attribute for the given name" do
-        subject.should eql(User.arel_table[:username])
-      end
-      
-      it { should be_extended_by(SexyScopes::Arel::ExpressionWrappers) }
+  describe ".attribute(name)" do
+    subject { User.attribute(:username) }
+    
+    it "should return an Arel attribute for the given name" do
+      subject.should eql User.arel_table[:username]
     end
     
-    describe ".literal(expression)" do
-      subject { User.literal('NOW()') }
-      
-      it "should return an Arel literal for given expression" do
-        subject.should eql(::Arel.sql('NOW()'))
-      end
-      
-      it { should be_extended_by(SexyScopes::Arel::ExpressionWrappers) }
-      
-      it { should be_extended_by(SexyScopes::Arel::PredicateWrappers) }
+    it { should be_extended_by SexyScopes::Arel::ExpressionWrappers }
+  end
+  
+  describe ".sql_literal(expression)" do
+    subject { User.sql_literal('NOW()') }
+    
+    it "should return an Arel literal for given expression" do
+      subject.should eql(::Arel.sql('NOW()'))
     end
+    
+    it "should be aliased as `sql`" do
+      SexyScopes::ActiveRecord.instance_method(:sql).should ==
+        SexyScopes::ActiveRecord.instance_method(:sql_literal)
+    end
+    
+    it { should be_extended_by SexyScopes::Arel::ExpressionWrappers }
+    
+    it { should be_extended_by SexyScopes::Arel::PredicateWrappers }
   end
   
   protected
@@ -35,5 +38,4 @@ describe SexyScopes::ActiveRecord do
     rescue NameError
       Arel.const_get(name)
     end
-
 end
