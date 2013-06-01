2c6557759baabece7fcda7393537f67b4197bae9
Samuel Lebeau <samuel.lebeau@gmail.com>
2013-06-01 18:07:37 +0200

    Split active_record_spec.rb into 2 individual files to reflect "lib" hierarchy

--- spec/active_record/dynamic_methods_spec.rb
+++ spec/active_record/dynamic_methods_spec.rb
@@ -1,37 +1,5 @@
 require 'spec_helper'
 
-describe SexyScopes::ActiveRecord::ClassMethods do
-  it "should extend ActiveRecord::Base" do
-    ActiveRecord::Base.should be_extended_by described_class
-  end
-  
-  describe ".attribute(name)" do
-    subject { User.attribute(:username) }
-    
-    it "should return an Arel attribute for the given name" do
-      subject.should == User.arel_table[:username]
-    end
-    
-    it { should be_extended_by SexyScopes::Arel::ExpressionMethods }
-  end
-  
-  describe ".sql_literal(expression)" do
-    subject { User.sql_literal('NOW()') }
-    
-    it "should return an Arel literal for given expression" do
-      subject.should == ::Arel.sql('NOW()')
-    end
-    
-    it "should be aliased as `sql`" do
-      described_class.instance_method(:sql).should == described_class.instance_method(:sql_literal)
-    end
-    
-    it { should be_extended_by SexyScopes::Arel::ExpressionMethods }
-    
-    it { should be_extended_by SexyScopes::Arel::PredicateMethods }
-  end
-end
-
 describe SexyScopes::ActiveRecord::DynamicMethods do
   before do
     ActiveRecord::Migration.create_table :temp_users
