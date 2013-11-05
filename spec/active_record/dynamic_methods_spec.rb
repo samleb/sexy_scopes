bb2bbb5680ca01beb89d85ba5078076e5f56b73e
Samuel Lebeau <samuel.lebeau@gmail.com>
2013-11-05 11:40:28 +0100

    More demanding/precise specs for ActiveRecord::DynamicMethods

--- spec/active_record/dynamic_methods_spec.rb
+++ spec/active_record/dynamic_methods_spec.rb
@@ -2,9 +2,20 @@ require 'spec_helper'
 
 describe SexyScopes::ActiveRecord::DynamicMethods do
   before do
-    ActiveRecord::Migration.create_table :temp_users
-    ActiveRecord::Migration.add_column :temp_users, :username, :string
-    class ::TempUser < ActiveRecord::Base; end
+    ActiveRecord::Migration.create_table :temp_users do |t|
+      t.string :username
+      t.string :name
+      t.string :foo
+      t.integer :score
+    end
+    
+    class ::TempUser < ActiveRecord::Base
+      # Deliberately defining a method whose name is a column
+      # This shouldn't be overwriten by the system
+      def self.foo
+        :bar
+      end
+    end
   end
   
   after do
@@ -13,28 +24,64 @@ describe SexyScopes::ActiveRecord::DynamicMethods do
   end
   
   it "should delegate to `attribute` when the method name is the name of an existing column" do
-    TempUser.should respond_to(:username)
     TempUser.should_receive(:attribute).with('username').once.and_return(:ok)
     TempUser.username.should == :ok
   end
   
-  it "should define an attribute method to avoid repeated `method_missing` calls" do
+  it "should define attribute methods all at once, avoiding repeated `method_missing` calls" do
     TempUser.username
     TempUser.should_not_receive(:method_missing)
     TempUser.username
+    TempUser.score
   end
   
-  it "should return a Method object for an existing column" do
+  it "should not override existing methods" do
+    TempUser.foo.should == :bar
+    TempUser.name.should == "TempUser"
+  end
+  
+  it "should respond to methods whose names are column names" do
+    TempUser.should respond_to(:username)
+  end
+  
+  it "should return a Method object for methods whose names are column names" do
     expect { TempUser.method(:username) }.to_not raise_error
   end
   
-  it "should raise NoMethodError for a non-existing column" do
+  it "should raise `NoMethodError` for methods whose names aren't column names" do
     TempUser.should_not respond_to(:foobar)
     expect { TempUser.foobar }.to raise_error NoMethodError
   end
   
   it "should not raise error when table doesn't exist" do
     TempUser.table_name = "inexistent_users"
-    expect { TempUser.respond_to?(:username) }.to_not raise_error
+    expect { TempUser.username }.to raise_error NoMethodError
+  end
+  
+  shared_examples "a non-table class" do
+    it "should not raise error when calling `respond_to?`" do
+      expect { subject.respond_to?(:username) }.not_to raise_error
+    end
+    
+    it "should raise `NoMethodError` error when going through `method_missing`" do
+      expect { subject.username }.to raise_error NoMethodError
+    end
+  end
+  
+  context "when the table doesn't exist" do
+    before { TempUser.table_name = "inexistent_users" }
+    subject { TempUser }
+    it_behaves_like "a non-table class"
+  end
+  
+  context "when the class is an abstract class" do
+    before { TempUser.abstract_class = true }
+    subject { TempUser }
+    it_behaves_like "a non-table class"
+  end
+  
+  context "when the class is `ActiveRecord::Base`" do
+    subject { ActiveRecord::Base }
+    it_behaves_like "a non-table class"
   end
 end
