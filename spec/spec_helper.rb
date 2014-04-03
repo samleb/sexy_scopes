c25daa0f6f9278fd62eb48a93bc3fa700567da0e
Samuel Lebeau <samuel.lebeau@gmail.com>
2014-04-03 12:53:48 +0200

    Fix typo in shared_examples' class name regular expressions

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -23,7 +23,7 @@ end
 
 shared_examples "a predicate method" do
   it "should return an Arel node" do
-    subject.class.name.should =~ /(^|SexyScopes::)Arel::Nodes::/
+    subject.class.name.should =~ /^(SexyScopes::)?Arel::Nodes::/
   end
   
   it { should be_extended_by SexyScopes::Arel::PredicateMethods }
@@ -31,7 +31,7 @@ end
 
 shared_examples "an expression method" do
   it "should return an Arel node" do
-    subject.class.name.should =~ /(^|SexyScopes::)Arel::Nodes::/
+    subject.class.name.should =~ /^(SexyScopes::)?Arel::Nodes::/
   end
   
   it { should be_extended_by SexyScopes::Arel::ExpressionMethods }
