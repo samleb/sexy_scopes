6483ce9bd9dceccf334a038e1b8d4bc212f5c4d0
Samuel Lebeau <samuel.lebeau@gmail.com>
2014-06-29 04:06:49 +0200

    Remove logging facility in specs (for now)

--- spec/support/connection.rb
+++ spec/support/connection.rb
@@ -1,5 +1,3 @@
-require 'active_support/logger'
-
 module SexyScopesSpec
   extend self
   
@@ -15,7 +13,6 @@ module SexyScopesSpec
   
   def connect
     puts "Connecting using #{database_system}"
-    ActiveRecord::Base.logger = ActiveSupport::Logger.new("debug.log", 0, 10 * 1024 * 1024)
     ActiveRecord::Base.establish_connection connection_config
   end
 end
