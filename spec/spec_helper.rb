f09686a60d24d610d66298beac63a2166f894d7b
Samuel Lebeau <samuel.lebeau@gmail.com>
2014-06-29 02:07:25 +0200

    Move shared examples to spec/support/shared_examples.rb

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -17,22 +17,6 @@ require 'rspec'
 require 'active_record'
 require 'sexy_scopes'
 
-Dir.glob(File.join(File.dirname(__FILE__), '{fixtures,matchers}', '*')) do |file|
+Dir.glob(File.join(File.dirname(__FILE__), '{support,fixtures,matchers}', '*')) do |file|
   require file
-end
-
-shared_examples "a predicate method" do
-  it "should return an Arel node" do
-    subject.class.name.should =~ /^(SexyScopes::)?Arel::Nodes::/
-  end
-  
-  it { should be_extended_by SexyScopes::Arel::PredicateMethods }
-end
-
-shared_examples "an expression method" do
-  it "should return an Arel node" do
-    subject.class.name.should =~ /^(SexyScopes::)?Arel::Nodes::/
-  end
-  
-  it { should be_extended_by SexyScopes::Arel::ExpressionMethods }
 end
