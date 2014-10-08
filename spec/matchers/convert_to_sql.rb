7544a5219c6fd209eb295a31acfe42afc93f25df
Samuel Lebeau <samuel.lebeau@augmentedev.com>
2014-10-08 12:33:46 +0200

    Normalize generated SQL in convert_to_sql matcher to avoid false positives

--- spec/matchers/convert_to_sql.rb
+++ spec/matchers/convert_to_sql.rb
@@ -1,24 +1,30 @@
 RSpec::Matchers.define :convert_to_sql do |expected|
+  case SexyScopesSpec.database_system
+  when 'mysql'
+    expected.gsub!('"', '`')
+  when 'postgresql'
+    expected.gsub!('LIKE', 'ILIKE')
+  end
+
   match do |actual|
-    actual.to_sql == convert_expected_sql(expected)
+    convert_to_sql(actual) == expected
   end
-  
+
   description do
-    "convert to the following SQL: #{convert_expected_sql(expected)}"
+    "convert to the following SQL: #{expected}"
   end
-  
+
   failure_message_for_should do |actual|
-    "expected generated SQL to be \n  #{convert_expected_sql(expected)}\ngot\n  #{actual.to_sql}"
+    "expected generated SQL to be \n  #{expected}\ngot\n  #{convert_to_sql(actual)}"
   end
-  
-  def convert_expected_sql(expected)
-    case SexyScopesSpec.database_system
-    when 'mysql'
-      expected.gsub('"', '`')
-    when 'postgresql'
-      expected.gsub('LIKE', 'ILIKE')
-    else
-      expected
-    end
+
+  def convert_to_sql(actual)
+    normalize_sql(actual.to_sql)
+  end
+
+  # Arel < 6 seems to inserts an additionnal (harmless) space before the WHERE clause.
+  # We ensure this space is removed to avoid false positives.
+  def normalize_sql(sql)
+    sql.gsub('  WHERE ', ' WHERE ')
   end
 end
