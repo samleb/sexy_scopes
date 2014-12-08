2e323602c3ff90306823e55e5cd5ea6e4325ac05
Thomas Kriechbaumer <kriechbaumer@gmail.com>
2014-12-08 13:48:32 +0000

    fix arel compatibility

--- lib/sexy_scopes/arel/predications.rb
+++ lib/sexy_scopes/arel/predications.rb
@@ -5,12 +5,12 @@ module SexyScopes
         SexyScopes.extend_predicate(super)
       end
       alias == eq
-      
+
       def in(other)
         SexyScopes.extend_predicate(super)
       end
-      
-      def matches(other)
+
+      def matches(other, *)
         if Regexp === other
           matches_regexp(other)
         else
@@ -18,8 +18,8 @@ module SexyScopes
         end
       end
       alias =~ matches
-      
-      def does_not_match(other)
+
+      def does_not_match(other, *)
         if Regexp === other
           does_not_match_regexp(other)
         else
@@ -27,36 +27,36 @@ module SexyScopes
         end
       end
       alias !~ does_not_match
-      
+
       def matches_regexp(other)
         predicate = Arel::Nodes::RegexpMatches.new(self, other)
         SexyScopes.extend_predicate(predicate)
       end
-      
+
       def does_not_match_regexp(other)
         matches_regexp(other).not
       end
-      
+
       def gteq(other)
         SexyScopes.extend_predicate(super)
       end
       alias >= gteq
-      
+
       def gt(other)
         SexyScopes.extend_predicate(super)
       end
       alias > gt
-      
+
       def lt(other)
         SexyScopes.extend_predicate(super)
       end
       alias < lt
-      
+
       def lteq(other)
         SexyScopes.extend_predicate(super)
       end
       alias <= lteq
-      
+
       def not_eq(other)
         SexyScopes.extend_predicate(super)
       end
