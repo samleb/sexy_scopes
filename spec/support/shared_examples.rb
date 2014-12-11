353994dc892547b577666a70286dacb936e3daef
Samuel Lebeau <samuel.lebeau@augmentedev.com>
2014-12-11 02:18:06 +0100

    Use new RSpec syntax for shared examples

--- spec/support/shared_examples.rb
+++ spec/support/shared_examples.rb
@@ -1,15 +1,15 @@
-shared_examples "a predicate method" do
+RSpec.shared_examples "a predicate method" do
   it "should return an Arel node" do
     subject.class.name.should =~ /^(SexyScopes::)?Arel::Nodes::/
   end
-  
+
   it { should be_extended_by SexyScopes::Arel::PredicateMethods }
 end
 
-shared_examples "an expression method" do
+RSpec.shared_examples "an expression method" do
   it "should return an Arel node" do
     subject.class.name.should =~ /^(SexyScopes::)?Arel::Nodes::/
   end
-  
+
   it { should be_extended_by SexyScopes::Arel::ExpressionMethods }
 end
